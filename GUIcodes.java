35b0742955ef1c7c6544827396bb7fa19d7c6021
Kyle Ronayne <34499017+kyleronayne@users.noreply.github.com>
2019-02-26 09:22:10 -0500

    Add files via upload

---
+++ GUIcodes.java
@@ -0,0 +1,6 @@
+package Project3;
+
+public enum GUIcodes {
+    NoMessage, NotYourTurn, inCheck, Checkmate
+
+}
---
+++ King.java
@@ -0,0 +1,18 @@
+package Project3;
+
+public class King extends ChessPiece {
+
+    public King(Player player) {
+        super(player);
+    }
+
+    public String type() {
+        return "King";
+    }
+
+    public boolean isValidMove(Move move, IChessPiece[][] board) {
+        boolean valid = true;
+        // More code is needed
+        return valid;
+    }
+}
