9ad226aa8a731782a5fac91d3bb15a9b3a92aee1
Charlie Dorn <charliedorn6@gmail.com>
2019-03-12 20:51:32 -0400

    Finished King class

--- ChessPrj/King.java
+++ ChessPrj/King.java
@@ -14,6 +14,23 @@ public class King extends ChessPiece {
         boolean superValid = super.isValidMove(move, board);
         boolean valid = false;
 
+        if((move.fromRow+1 == move.toRow)&&(move.fromColumn == move.toColumn))
+            valid = true;
+        else if((move.fromRow+1 == move.toRow)&&(move.fromColumn+1 == move.toColumn))
+            valid = true;
+        else if((move.fromRow == move.toRow)&&(move.fromColumn+1 == move.toColumn))
+            valid = true;
+        else if((move.fromRow-1 == move.toRow)&&(move.fromColumn+1 == move.toColumn))
+            valid = true;
+        else if((move.fromRow-1 == move.toRow)&&(move.fromColumn == move.toColumn))
+            valid = true;
+        else if((move.fromRow-1 == move.toRow)&&(move.fromColumn-1 == move.toColumn))
+            valid = true;
+        else if((move.fromRow == move.toRow)&&(move.fromColumn-1 == move.toColumn))
+            valid = true;
+        else if((move.fromRow+1 == move.toRow)&&(move.fromColumn-1 == move.toColumn))
+            valid = true;
+
         return valid&&superValid;
     }
 }
