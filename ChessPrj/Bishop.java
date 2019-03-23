dc47a0f41a5ea587605943d46fe2c7f59b46af1e
perpetuallink <Samack1011@aim.com>
2019-03-23 11:16:40 -0400

    Commented out rest of bishop

--- ChessPrj/Bishop.java
+++ ChessPrj/Bishop.java
@@ -33,35 +33,51 @@ public class Bishop extends ChessPiece {
      * @param board
      * @return A boolean representing whether the specified Move is
      * valid
-     */
+     *****************************************************************/
     public boolean isValidMove(Move move, IChessPiece[][] board) {
+
+        //Is the move fundamentally valid?
         boolean superValid = super.isValidMove(move, board);
 
+        // place holder that holds whether the move is valid or not
         boolean valid = false;
-        if((move.fromRow == move.toRow) || (move.fromColumn == move.toColumn))
+
+        /* Is the attempted move in the 4 cardinal directions
+         Up, Down, Left, or Right */
+        if((move.fromRow == move.toRow) || (move.fromColumn ==
+                move.toColumn))
             valid = false;
 
+        /* Is the attempted move a diagonal movement. Does the piece
+         change the same number of rows and columns*/
         if((Math.abs(move.fromRow - move.toRow) == Math.abs(move.fromColumn - move.toColumn))) {
             valid = true;
 
+            // Attempted move is up and to the left
             if((move.fromRow > move.toRow) && (move.fromColumn > move.toColumn)) {
                 for(int i=1; i<Math.abs(move.fromRow - move.toRow); i++)    {
                     if(!(board[move.fromRow-i][move.fromColumn-i] == null))
                         return false;
                 }
             }
+
+            // Attempted move is down and to the right
             else if((move.fromRow < move.toRow) && (move.fromColumn < move.toColumn)) {
                 for(int i=1; i<Math.abs(move.fromRow - move.toRow); i++)    {
                     if(!(board[move.fromRow+i][move.fromColumn+i] == null))
                         return false;
                 }
             }
+
+            // Attempted move is up and to the right
             else if((move.fromRow > move.toRow) && (move.fromColumn < move.toColumn)) {
                 for(int i=1; i<Math.abs(move.fromRow - move.toRow); i++)    {
                     if(!(board[move.fromRow-i][move.fromColumn+i] == null))
                         return false;
                 }
             }
+
+            // Attempted move is down and to the left
             else if((move.fromRow < move.toRow) && (move.fromColumn > move.toColumn)) {
                 for(int i=1; i<Math.abs(move.fromRow - move.toRow); i++)    {
                     if(!(board[move.fromRow+i][move.fromColumn-i] == null))
